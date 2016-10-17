c334fb1aabbf0fccf3bf160121e07ad7d3e1ba10
RodjFlores <rjf26@njit.edu>
2016-10-17 15:51:23 -0400

    TRIED TO MAKE AN ARRAY

--- table.php
+++ table.php
@@ -1,2 +1,34 @@
-x
-Q
+<?php
+
+$nums = array('one' => 1, 'two' => 2, 'three' => 3);
+
+echo $nums['one'].' SPACE SPACE TEST SPACE'.$nums['two'];
+
+?>
+
+
+
+
+
+
+
+<html>
+<body>
+<table border='1' cellspacing='1'>
+<tbody>
+
+
+ <?php foreach ($nums as $word => $digit){
+                
+                
+                echo'<td>'. $word[1].'</td>';
+                echo'<td>'. $word[0].'</td>';
+                echo'<td>'. $word[0].'</td>';
+                echo'<tr>';
+                
+              }
+            ?>
+</tbody
+</table>
+</body>
+</html>
